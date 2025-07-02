68013aa8a409ccd4761c78d244d07060809e8ef1
Fathur Rizqi <fathurrizqizdjafar@gmail.com>
2025-07-03 00:50:45 +0800

    update

--- app.py
+++ app.py
@@ -92,21 +92,36 @@ def upload_tugas():
         return redirect(url_for('upload_tugas'))
 
     # Ambil daftar tugas
-    tugas_list = list(db.tugas_collection.find())
-    return render_template('dashboard_dosen_laporan.html', tugas_dosen=tugas_list)
-
+    
+    return render_template('dashboard_dosen_laporan.html')
+@app.route('/daftar_tugas', methods=['GET', 'POST'])
+def daftar_tugas():
+    
+    return render_template('dashboard_dosen_daftar_tugas.html')
 
 # Route untuk akses file
 @app.route('/uploads/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
+@app.route('/mahasiswa/laporan')
+def lihat_laporan():
+    if session.get('role') != 'mahasiswa':
+        return redirect(url_for('home'))
+
+
+    
+    laporan = db.tugas_collection.find()
+
+    return render_template('dashboard_mahasiswa_daftarTugas.html', laporan=laporan)
 
 @app.route('/admin')
 def admin_dashboard():
     if session.get('role') != 'admin':
         return redirect(url_for('home'))
-    return render_template('dashboard_admin.html')  
+    daftar_dosen = db.users.find({'role':'Dosen'})
+    matakuliah = db.mata_kuliah.find()
+    return render_template('dashboard_admin.html',daftar_dosen=daftar_dosen,mata_kuliah=matakuliah)  
 
 @app.route('/admin/tambah-jadwal', methods=['POST'])
 def tambah_jadwal():
@@ -146,59 +161,90 @@ def data_dosen():
 def tambah_dosen():
     # Untuk saat ini hanya redirect ulang, simpan data di database jika ingin dinamis
     return redirect(url_for('data_dosen'))
-# @app.route('/add_user', methods=['GET', 'POST'])
-# def add_user():
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         email = request.form['email']
-#         jurusan = request.form['jurusan']
-#         nim = request.form['nim']
+@app.route('/admin/mata-kuliah')
+def data_mata_kuliah():
+    if session.get('role') != 'admin':
+        return redirect(url_for('home'))
 
-#         # Jika role Mahasiswa, nidn kosongkan
+    mata_kuliah = db.mata_kuliah.find()
+    return render_template('admin_data_matkul.html', mata_kuliah=mata_kuliah)
+@app.route('/admin/tambah-mata-kuliah', methods=['POST'])
+def tambah_mata_kuliah():
+    if session.get('role') != 'admin':
+        return redirect(url_for('home'))
+
+    kode = request.form['kode']
+    nama = request.form['nama']
+    jurusan = request.form['jurusan']
+
+    mk = {
+        'kode': kode,
+        'nama': nama,
+        'jurusan': jurusan
+    }
+    db.mata_kuliah.insert_one(mk)
+    return redirect(url_for('data_mata_kuliah'))
+@app.route('/admin/hapus-mata-kuliah/<id>', methods=['POST'])
+def hapus_mata_kuliah(id):
+    if session.get('role') != 'admin':
+        return redirect(url_for('home'))
+
+    db.mata_kuliah.delete_one({'_id': ObjectId(id)})
+    return redirect(url_for('data_mata_kuliah'))
+
+@app.route('/add_user', methods=['GET', 'POST'])
+def add_user():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        jurusan = request.form['jurusan']
+        nim = request.form['nim']
+
+        # Jika role Mahasiswa, nidn kosongkan
         
 
-#         user = {
-#             'name': name,
-#             'email': email,
-#             'passwordHash': nim,
-#             'jurusan': jurusan,
-#             'role': 'Mahasiswa',
-#             'nim': nim,
-#             'nidn': '',
-#             'createdAt': datetime.utcnow()
-#         }
+        user = {
+            'name': name,
+            'email': email,
+            'passwordHash': nim,
+            'jurusan': jurusan,
+            'role': 'Mahasiswa',
+            'nim': nim,
+            'nidn': '',
+            'createdAt': datetime.utcnow()
+        }
 
-# #         db.users.insert_one(user)
-# #         return redirect(url_for('data_mahasiswa'))
+        db.users.insert_one(user)
+        return redirect(url_for('data_mahasiswa'))
 
-# #     return render_template('add_user.html')
+    return render_template('add_user.html')
 
-# @app.route('/add_user', methods=['GET', 'POST'])
-# def add_user():
-#     if request.method == 'POST':
-#         name = request.form['name']
-#         email = request.form['email']
-#         jurusan = request.form['jurusan']
-#         nim = request.form['nipn']
+@app.route('/add_user_dosen', methods=['GET', 'POST'])
+def add_user_dosen():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        jurusan = request.form['jurusan']
+        nim = request.form['nipn']
 
-#         # Jika role Mahasiswa, nidn kosongkan
+        # Jika role Mahasiswa, nidn kosongkan
         
 
-#         user = {
-#             'name': name,
-#             'email': email,
-#             'passwordHash': nim,
-#             'jurusan': jurusan,
-#             'role': 'Mahasiswa',
-#             'nim': nim,
-#             'nidn': '',
-#             'createdAt': datetime.utcnow()
-#         }
+        user = {
+            'name': name,
+            'email': email,
+            'passwordHash': nim,
+            'jurusan': jurusan,
+            'role': 'Mahasiswa',
+            'nim': nim,
+            'nidn': '',
+            'createdAt': datetime.utcnow()
+        }
 
-#         db.users.insert_one(user)
-#         return redirect(url_for('data_mahasiswa'))
+        db.users.insert_one(user)
+        return redirect(url_for('data_dosen'))
 
-#     return render_template('add_user.html')
+    return render_template('admin_data_dosen.html')
 
 
 if __name__ == '__main__':
